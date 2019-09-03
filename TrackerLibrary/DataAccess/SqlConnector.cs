e7eefbb9e57b8329710eabbf772ac6ed655b2644
rashalismath2 <rashalismath@gmail.com>
2019-09-03 15:54:05 +0530

    Changed library structure

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.Models;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.DataAccess
 {   
     /// <summary>
     ///     Implelements IDataConnection Interface to add data into sql database
